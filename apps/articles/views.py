6b42d3cf15badfb0e9f0752057b7e1fdc7080d4b
Sergey Dyachok <sergey@sdyachok.com.ua>
2020-07-11 12:19:19 +0300

    Model generic views
    - Added Article list and detail views
    - Added articles urls.py

--- apps/articles/views.py
+++ apps/articles/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from django.views.generic import ListView, DetailView
 
-# Create your views here.
+from .models import Article
+
+
+class ArticleListView(ListView):
+    model = Article
+    context_object_name = "articles"
+
+
+class ArticleDetailView(DetailView):
+    model = Article
+    context_object_name = "article"
