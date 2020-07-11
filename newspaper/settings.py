856a54de7909d87aef82c8fb1ab62f6aa37fe9c3
Sergey Dyachok <sergey@sdyachok.com.ua>
2020-07-11 21:53:07 +0300

    Context processors
    - Added articles context_processors.py
    - Updated settings.py
    - Updated header template

--- newspaper/settings.py
+++ newspaper/settings.py
@@ -68,10 +68,13 @@ TEMPLATES = [
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
+                # Django context processors
                 "django.template.context_processors.debug",
                 "django.template.context_processors.request",
                 "django.contrib.auth.context_processors.auth",
                 "django.contrib.messages.context_processors.messages",
+                # Project context processors
+                "apps.articles.context_processors.categories",
             ],
         },
     },
