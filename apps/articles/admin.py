69b81b426642906eb0ad0323dff316f75e4b3fed
Sergey Dyachok <sergey@sdyachok.com.ua>
2020-07-11 16:11:21 +0300

    Model relations
    - Added Category model
    - Added category to Article model
    - Added admin for Category
    - Updated list and detail templates

--- apps/articles/admin.py
+++ apps/articles/admin.py
@@ -1,11 +1,13 @@
 from django.contrib import admin
 
-from .models import Article
+from .models import Article, Category
 
 
 @admin.register(Article)
 class ArticleAdmin(admin.ModelAdmin):
     date_hierarchy = "created_at"
+
+    ordering = ("-created_at",)
 
     prepopulated_fields = {"slug": ("title",)}
 
@@ -16,3 +18,13 @@ class ArticleAdmin(admin.ModelAdmin):
     list_display = ("pk", "title", "is_published", "published_at", "created_at")
     list_display_links = ("pk", "title")
     list_filter = ("is_published",)
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    ordering = ("name",)
+
+    prepopulated_fields = {"slug": ("name",)}
+
+    list_display = ("pk", "name", "created_at")
+    list_display_links = ("pk", "name")
--- apps/articles/models.py
+++ apps/articles/models.py
@@ -1,12 +1,37 @@
 from django.db import models
 
 
+class Category(models.Model):
+    name = models.CharField(verbose_name="name", max_length=50, unique=True)
+    slug = models.SlugField(max_length=50)
+    created_at = models.DateTimeField(verbose_name="created at", auto_now_add=True)
+    updated_at = models.DateTimeField(verbose_name="updated at", auto_now=True)
+
+    class Meta:
+        verbose_name = "category"
+        verbose_name_plural = "categories"
+
+    def __str__(self):
+        return self.name
+
+    def __repr__(self):
+        return "<Category id={} name={}>".format(self.pk, self.name)
+
+
 class Article(models.Model):
     title = models.CharField(
         verbose_name="title", max_length=150, blank=False, null=False
     )
     slug = models.SlugField(
         max_length=160, blank=True, null=False, unique_for_date="published_at"
+    )
+    category = models.ForeignKey(
+        Category,
+        verbose_name="category",
+        blank=False,
+        null=True,
+        on_delete=models.SET_NULL,
+        related_name="articles",
     )
     description = models.TextField(verbose_name="description", max_length=600)
     content = models.TextField(verbose_name="content")
