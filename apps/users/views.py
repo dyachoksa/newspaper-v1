cc8a18b240b21f3201c46c73fc68e667e7a71e93
Sergey Dyachok <sergey@sdyachok.com.ua>
2020-07-12 12:56:38 +0300

    Auth: Logout
    - Added logout view
    - Added logout url
    - Updated header

--- apps/users/views.py
+++ apps/users/views.py
@@ -4,3 +4,7 @@ from django.contrib.auth import views as auth_views
 class LoginView(auth_views.LoginView):
     template_name = "users/login.html"
     redirect_authenticated_user = True
+
+
+class LogoutView(auth_views.LogoutView):
+    pass
