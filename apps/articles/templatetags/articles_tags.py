c0f392740f8c3c9c34f67bcba644553eac4215ee
Sergey Dyachok <sergey@sdyachok.com.ua>
2020-07-13 23:23:12 +0300

    Django template tags
    - Added article tags
    - Added most popular articles template
    - Updated article templates
    - Updated home page template

---
+++ apps/articles/templatetags/articles_tags.py
@@ -0,0 +1,12 @@
+from django import template
+
+from ..models import Article
+
+register = template.Library()
+
+
+@register.inclusion_tag("articles/tags/most_popular_articles.html")
+def most_popular_articles():
+    articles = Article.published.order_by("-comments_count").all()[:5]
+
+    return {"articles": articles}
